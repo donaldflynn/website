e30e459699c727db50dd01e23c964ffd2b735a57
donald <donald.flynn@skyrad.co.uk>
2024-05-01 22:27:01 +0100

    Fixed eslint issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import {useEffect} from "react";
+import {Route, Routes} from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
-import ReadArticle from "./pages/readArticle";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
+import {TRACKING_ID} from "./data/tracking";
 import "./app.css";
 
 function App() {
